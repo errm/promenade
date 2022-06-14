f498792b8c5ba7aa6014d3a47c4b34512bcffcf9
Gavin Morrice <gavin@gavinmorrice.com>
2022-06-14 15:47:40 +0100

    Tidy dummy app

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -7,12 +7,12 @@ require_relative "boot"
   require railtie
 end
 
-require "promenade/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 require "promenade"
+require "promenade/railtie"
 
 module Dummy
   class Application < Rails::Application
@@ -20,13 +20,5 @@ module Dummy
 
     # For compatibility with applications that use this config
     config.action_controller.include_all_helpers = false
-
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
