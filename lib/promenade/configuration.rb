f36fa21a12e55e0b7108259fa7082032a5fd91cb
Gavin Morrice <gavin@gavinmorrice.com>
2022-08-12 09:54:46 +0100

    Reduce no. of default time duration buckets

--- lib/promenade/configuration.rb
+++ lib/promenade/configuration.rb
@@ -4,8 +4,7 @@ module Promenade
 
     DEFAULT_RACK_LATENCY_BUCKETS = [0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1, 2.5, 5, 10].freeze
 
-    # 2ms to 20ms in intervals of 2ms
-    DEFAULT_QUEUE_TIME_BUCKETS = [0.002, 0.004, 0.006, 0.008, 0.01, 0.012, 0.014, 0.016, 0.018, 0.02].freeze
+    DEFAULT_QUEUE_TIME_BUCKETS = [0.01, 0.5, 1.0, 10.0, 30.0].freeze
 
     def initialize
       @rack_latency_buckets = DEFAULT_RACK_LATENCY_BUCKETS
