bb637267a0cbaa9e7b34a71fea6247765feee077
Alina Hryshchuk <alina.grishchuk91@gmail.com>
2023-02-27 14:02:53 +0000

    basic consumer classes

---
+++ lib/promenade/karafka/processors/consumer_processor.rb
@@ -0,0 +1,44 @@
+module Promenade
+  module Processors
+    class ConsumerProcessor
+      def self.call(event)
+        # TODO: uncomeent after defining labels
+        # new(event).call
+      end
+
+      def initialize(event)
+        @event = event
+      end
+
+      def call
+        send_metrics
+      end
+
+      private
+
+        attr_reader :event
+
+        def send_metrics
+          Promenade.metric(:kafka_consumer_messages_processed).increment(labels, messages.count)
+          Promenade.metric(:kafka_consumer_batch_processing_latency).observe(labels, event.duration)
+        end
+
+        def consumer
+          @_consumer = event.payload[:caller]
+        end
+
+        def messages
+          @_messages = consumer.messages
+        end
+
+        def metadata
+          @_metadata = messages.metadata
+        end
+
+        # TODO: implement
+        def labels
+          {}
+        end
+    end
+  end
+end
