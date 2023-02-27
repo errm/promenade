5d8a2c655608be57008a8c82a2edf260ffc8a069
Alina Hryshchuk <alina.grishchuk91@gmail.com>
2023-02-27 15:03:31 +0000

    fix namespacing

--- lib/promenade/karafka/error_subscriber.rb
+++ lib/promenade/karafka/error_subscriber.rb
@@ -1,17 +1,19 @@
 require "promenade/karafka/subscriber"
 
 module Promenade
-  class ErrorSubscriber < Subscriber
-    attach_to "error.kafka"
+  module Karafka
+    class ErrorSubscriber < Subscriber
+      attach_to "error.kafka"
 
-    def occurred(event)
-      error = {
-        error: event.payload[:error],
-        client: Karafka::App.config.client_id
-      }
+      def occurred(event)
+        error = {
+          error: event.payload[:error],
+          client: Karafka::App.config.client_id
+        }
 
-      Rails.logger.error "[Error] error occurred: #{error.inspect}"
-      Rails.logger.error "[Error] error inspected: #{event.payload.inspect}"
+        Rails.logger.error "[Error] error occurred: #{error.inspect}"
+        Rails.logger.error "[Error] error inspected: #{event.payload.inspect}"
+      end
     end
   end
 end
