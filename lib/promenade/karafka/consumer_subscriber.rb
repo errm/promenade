a13c00df396412a3caf44c229cbf06f8deb6fa15
Alina Hryshchuk <alina.grishchuk91@gmail.com>
2023-02-27 15:03:31 +0000

    fix namespacing

--- lib/promenade/karafka/consumer_subscriber.rb
+++ lib/promenade/karafka/consumer_subscriber.rb
@@ -2,18 +2,20 @@ require "promenade/karafka/subscriber"
 require "promenade/karafka/processors/consumer_processor"
 
 module Promenade
-  class ConsumerSubscriber < Subscriber
-    attach_to "consumer.kafka"
+  module Karafka
+    class ConsumerSubscriber < Subscriber
+      attach_to "consumer.kafka"
 
-    def consumed(event)
-      messages = event.payload[:caller].messages.map do |m|
-        { type: m.payload["type"], event_time: m.payload["event_time"],
-          id: m.payload.dig("body", "id") || "missing id for #{m.payload.inspect}" }
+      def consumed(event)
+        messages = event.payload[:caller].messages.map do |m|
+          { type: m.payload["type"], event_time: m.payload["event_time"],
+            id: m.payload.dig("body", "id") || "missing id for #{m.payload.inspect}" }
+        end
+
+        Logger.new($stdout).info "[kafka] consumed : #{messages.inspect}"
+
+        # Processors::ConsumerProcessor.call(event)
       end
-
-      Logger.new($stdout).info "[kafka] consumed : #{messages.inspect}"
-
-      Processors::ConsumerProcessor.call(event)
     end
   end
 end
