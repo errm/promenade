5b2cd53ab47d3c51881e429b861b1a45fc930548
Alina Hryshchuk <alina.grishchuk91@gmail.com>
2023-02-28 11:53:36 +0000

    update naming

--- lib/promenade/waterdrop/message_subscriber.rb
+++ lib/promenade/waterdrop/message_subscriber.rb
@@ -14,17 +14,11 @@ module Promenade
       def produced_sync(event)
         data = event.payload[:message].slice(:key, :topic).merge(producer_id: event.payload[:producer_id])
 
-        Rails.logger.info("[waterdrop] produced_async: #{data.inspect}")
-      end
-
-      def produced_async(event)
-        data = event.payload[:message].slice(:key, :topic).merge(producer_id: event.payload[:producer_id])
-
         Rails.logger.info("[waterdrop] produced_sync: #{data.inspect}")
       end
 
       def acknowledged(event)
-        Rails.logger.info "[kafka] message acknowledged: #{event.payload.inspect}"
+        Rails.logger.info "[waterdrop] message acknowledged: #{event.payload.inspect}"
       end
     end
   end
