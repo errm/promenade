26792585d898beac06452d9e2051cad24726b89d
Danny Dawson <danny-dawson@cookpad.com>
2023-03-06 17:16:10 +0000

    Updated specs

--- spec/karafka_spec.rb
+++ spec/karafka_spec.rb
@@ -14,8 +14,10 @@ RSpec.describe Promenade::Karafka do
   end
 
   describe "consumer.karafka" do
+    # rubocop:disable Lint/StructNewOverride:
     let(:metadata) { Struct.new(:partition, :topic).new(partition, topic_name) }
     let(:messages) { Struct.new(:size, :metadata).new(messages_size, metadata) }
+    # rubocop:enable Lint/StructNewOverride:
     let(:topic) { Struct.new(:consumer_group, :kafka).new(Struct.new(:id).new(consumer_group_id), { "client.id": client_id }) }
     let(:consumer) { Struct.new(:messages, :topic).new(messages, topic) }
     let(:messages_size) { 8 }
