3dfb6e4444f19cdae6601682ac872f5992eadfd3
Ed Robinson <edward-robinson@cookpad.com>
2024-02-26 11:05:15 +0000

    Fix tests across all tested versions of ruby

--- spec/yjit_spec.rb
+++ spec/yjit_spec.rb
@@ -3,9 +3,12 @@ require "promenade/yjit/stats"
 RSpec.describe Promenade::YJIT::Stats do
   describe "recording yjit stats" do
     it "records code_region_size" do
-      if defined? RubyVM::YJIT
+      # This method should not blow up in any case
+      expect { described_class.instrument }.not_to raise_error
 
-        # We want to test that this doesn't blow up when yjit isn't enabled
+      if defined? RubyVM::YJIT && defined? RubyVM::YJIT.enable
+
+        # We want to test that this doesn't blow up when yjit is present but isn't enabled yet
         # you need to run the testsuite with yjit disabled for this to work
         expect(RubyVM::YJIT.enabled?).to be_falsey
         expect { described_class.instrument }.not_to raise_error
