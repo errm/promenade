6e0470b9ddcb6bc5168da4d96489b1599c5e7c88
Roberto Miranda <rjmaltamar@gmail.com>
2024-08-20 10:15:47 +0100

    Add Memory Stats

---
+++ lib/promenade/pitchfork/mem_stats.rb
@@ -0,0 +1,37 @@
+begin
+  require "pitchfork/mem_info"
+rescue LoadError
+  # No pitchfork available, dont do anything
+end
+
+module Promenade
+  module Pitchfork
+    class MemStats
+
+      Promenade.gauge :pitchfork_mem_rss do
+        doc "Resident Set Size of the pitchfork process, Total memory used by the process."
+      end
+
+      Promenade.gauge :pitchfork_shared_mem do
+        doc "Shared memory of the pitchfork process, memory that is shared between multiple processes."
+      end
+
+      def initialize
+        return unless defined?(::Pitchfork) && defined?(::Pitchfork::MemInfo)
+
+        @mem_info = ::Pitchfork::MemInfo.new(Process.pid)
+        @parent_mem_info = ::Pitchfork::MemInfo.new(Process.ppid)
+      end
+
+      def instrument
+        Promenade.metric(:pitchfork_mem_rss).set({}, @mem_info.rss)
+        Promenade.metric(:pitchfork_shared_mem).set({}, @mem_info.shared_memory)
+      end
+
+      def self.instrument
+        new.instrument
+      rescue StandardError => e
+      end
+    end
+  end
+end
