69abfb09f86f1e39fac102109cfe4cbe8e13c78c
Roberto Miranda <rjmaltamar@gmail.com>
2024-08-22 15:37:33 +0100

    Conditionally use rack middlewares

--- lib/promenade/configuration.rb
+++ lib/promenade/configuration.rb
@@ -1,6 +1,6 @@
 module Promenade
   class Configuration
-    attr_accessor :queue_time_buckets, :rack_latency_buckets
+    attr_accessor :queue_time_buckets, :rack_latency_buckets, :pitchfork_stats_enabled
 
     DEFAULT_RACK_LATENCY_BUCKETS = [0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1, 2.5, 5, 10].freeze
 
@@ -9,6 +9,7 @@ module Promenade
     def initialize
       @rack_latency_buckets = DEFAULT_RACK_LATENCY_BUCKETS
       @queue_time_buckets = DEFAULT_QUEUE_TIME_BUCKETS
+      @pitchfork_stats_enabled = false
     end
   end
 end
