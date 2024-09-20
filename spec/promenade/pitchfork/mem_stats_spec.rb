4d843dd662addf03a2d9ac7f9b361fcd5ae179e5
Roberto Miranda <rjmaltamar@gmail.com>
2024-09-20 13:07:48 +0100

    Combine memory statistics into a single metric with labels
    Use base unit, bytes

--- spec/promenade/pitchfork/mem_stats_spec.rb
+++ spec/promenade/pitchfork/mem_stats_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe Promenade::Pitchfork::MemStats do
     stub_const("Pitchfork::MemInfo", pitfork_mem_info)
     allow(pitfork_mem_info).to receive(:new).and_return(pitfork_mem_info)
     allow(pitfork_mem_info).to receive(:rss).and_return(100)
+    allow(pitfork_mem_info).to receive(:pss).and_return(50)
     allow(pitfork_mem_info).to receive(:shared_memory).and_return(50)
   end
 
@@ -22,11 +23,11 @@ RSpec.describe Promenade::Pitchfork::MemStats do
     it "sets the metrics correctly" do
       stats = Promenade::Pitchfork::MemStats.new
 
-      expect(Promenade).to receive(:metric).with(:pitchfork_mem_rss).and_return(metric)
-      expect(Promenade).to receive(:metric).with(:pitchfork_shared_mem).and_return(metric)
+      expect(Promenade).to receive(:metric).with(:pitchfork_memory_usage_bytes).and_return(metric)
 
-      expect(metric).to receive(:set).with({}, 100)
-      expect(metric).to receive(:set).with({}, 50)
+      expect(metric).to receive(:set).with({ type: "RSS" }, 102400)
+      expect(metric).to receive(:set).with({ type: "PSS" }, 51200)
+      expect(metric).to receive(:set).with({ type: "Shared" }, 51200)
 
       stats.instrument
     end
