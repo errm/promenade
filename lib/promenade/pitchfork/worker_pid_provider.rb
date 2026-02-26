5e106c0e0d272eb37aab186f54174c7edf510ef4
Ed Robinson <edward-robinson@cookpad.com>
2026-02-26 15:46:51 +0000

    Fix pitchfork pid provider (#81)
    * Fix pitchfork pid provider
    
    * Test that we fallback to pid if we get an unexpected worker object

--- lib/promenade/pitchfork/worker_pid_provider.rb
+++ lib/promenade/pitchfork/worker_pid_provider.rb
@@ -5,26 +5,16 @@ module Promenade
         worker_id || "process_id_#{Process.pid}"
       end
 
-      def self.object_based_worker_id
+      def self.worker_id
         return unless defined?(::Pitchfork::Worker)
 
-        workers = ObjectSpace.each_object(::Pitchfork::Worker)
-        return if workers.nil?
-
-        workers_first = workers.first
-        workers_first&.nr
-      end
-
-      def self.program_name
-        $PROGRAM_NAME
-      end
-
-      def self.worker_id
-        if matchdata = program_name.match(/pitchfork.*worker\[(.+)\]/) # rubocop:disable Lint/AssignmentInCondition
-          "pitchfork_#{matchdata[1]}"
-        elsif object_worker_id = object_based_worker_id # rubocop:disable Lint/AssignmentInCondition
-          "pitchfork_#{object_worker_id}"
+        ObjectSpace.each_object(::Pitchfork::Worker) do |worker|
+          if worker.pid == Process.pid
+            return "worker_id_#{worker.nr}"
+          end
         end
+      rescue StandardError
+        nil
       end
     end
   end
--- spec/promenade/pitchfork/worker_pid_provider_spec.rb
+++ spec/promenade/pitchfork/worker_pid_provider_spec.rb
@@ -3,80 +3,48 @@ require "promenade/pitchfork/worker_pid_provider"
 
 RSpec.describe Promenade::Pitchfork::WorkerPidProvider do
   describe ".fetch" do
-    subject { described_class.fetch }
-
-    context "when worker_id is defined" do
-      let(:worker_id) { "worker_id" }
-
-      before do
-        allow(described_class).to receive(:worker_id).and_return(worker_id)
-      end
-
-      it { is_expected.to eq(worker_id) }
+    before do
+      allow(Process).to receive(:pid).and_return(123)
     end
 
-    context "when worker_id is not defined" do
+    subject { described_class.fetch }
+
+    context "fallback to pid when we cannot get the worker id" do
+      it { is_expected.to eq("process_id_123") }
+    end
+
+    context "when there are pitchfork workers" do
       before do
-        allow(described_class).to receive(:worker_id).and_return(nil)
-        allow(Process).to receive(:pid).and_return(123)
+        stub_const("Pitchfork::Worker", Class.new)
+        allow(ObjectSpace).to receive(:each_object).with(Pitchfork::Worker).
+          and_yield(double("Pitchfork::Worker", nr: 3, pid: 123)).
+          and_yield(double("Pitchfork::Worker", nr: 3, pid: 123)).
+          and_yield(double("Pitchfork::Worker", nr: 3, pid: 123))
+      end
+
+      it { is_expected.to eq("worker_id_3") }
+    end
+
+    context "pitchfork workers with mismatching pid" do
+      before do
+        stub_const("Pitchfork::Worker", Class.new)
+        allow(ObjectSpace).to receive(:each_object).with(Pitchfork::Worker).
+          and_yield(double("Pitchfork::Worker", nr: 3, pid: 122)).
+          and_yield(double("Pitchfork::Worker", nr: 3, pid: 122)).
+          and_yield(double("Pitchfork::Worker", nr: 3, pid: 122))
       end
 
       it { is_expected.to eq("process_id_123") }
     end
-  end
 
-  describe ".worker_id" do
-    subject { described_class.send(:worker_id) }
-    around(:example) do |ex|
-      old_name = $PROGRAM_NAME
-      $PROGRAM_NAME = program_name
-      ex.run
-      $PROGRAM_NAME = old_name
-    end
-
-    context "when program_name matches pitchfork worker" do
-      let(:program_name) { "pitchfork (gen:0) worker[1] - requests: 13, waiting" }
-
-
-      it { is_expected.to eq("pitchfork_1") }
-
-      context "when program_name matches pitchfork worker" do
-        let(:program_name) { "pitchfork worker[1]" }
-
-        it { is_expected.to eq("pitchfork_1") }
-      end
-
-      context "when program_name doesn't match pitchfork worker" do
-        let(:program_name) { "something else" }
-
-        let(:worker) { double("Pitchfork::Worker", nr: 2) }
-
-        before do
-          stub_const("Pitchfork::Worker", Class.new)
-          allow(ObjectSpace).to receive(:each_object).with(Pitchfork::Worker).and_return([worker])
-        end
-
-        it { is_expected.to eq("pitchfork_2") }
-      end
-    end
-  end
-
-  describe ".object_based_worker_id" do
-    subject { described_class.send(:object_based_worker_id) }
-
-    context "when Pitchfork::Worker is defined" do
-      let(:worker) { double("Pitchfork::Worker", nr: 1) }
-
+    context "unexpeced pitchfork worker object" do
       before do
         stub_const("Pitchfork::Worker", Class.new)
-        allow(ObjectSpace).to receive(:each_object).with(Pitchfork::Worker).and_return([worker])
+        allow(ObjectSpace).to receive(:each_object).with(Pitchfork::Worker).
+          and_yield(nil)
       end
 
-      it { is_expected.to eq(1) }
-    end
-
-    context "when Pitchfork::Worker is not defined" do
-      it { is_expected.to be_nil }
+      it { is_expected.to eq("process_id_123") }
     end
   end
 end
