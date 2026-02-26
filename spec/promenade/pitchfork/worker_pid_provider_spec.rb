7417db56fc8941b582d65184a158f0792b03b815
Ed Robinson <edward-robinson@cookpad.com>
2026-02-26 15:29:20 +0000

    Test that we fallback to pid if we get an unexpected worker object

--- spec/promenade/pitchfork/worker_pid_provider_spec.rb
+++ spec/promenade/pitchfork/worker_pid_provider_spec.rb
@@ -3,19 +3,18 @@ require "promenade/pitchfork/worker_pid_provider"
 
 RSpec.describe Promenade::Pitchfork::WorkerPidProvider do
   describe ".fetch" do
+    before do
+      allow(Process).to receive(:pid).and_return(123)
+    end
+
     subject { described_class.fetch }
 
     context "fallback to pid when we cannot get the worker id" do
-      before do
-        allow(Process).to receive(:pid).and_return(123)
-      end
-
       it { is_expected.to eq("process_id_123") }
     end
 
     context "when there are pitchfork workers" do
       before do
-        allow(Process).to receive(:pid).and_return(123)
         stub_const("Pitchfork::Worker", Class.new)
         allow(ObjectSpace).to receive(:each_object).with(Pitchfork::Worker).
           and_yield(double("Pitchfork::Worker", nr: 3, pid: 123)).
@@ -28,12 +27,21 @@ RSpec.describe Promenade::Pitchfork::WorkerPidProvider do
 
     context "pitchfork workers with mismatching pid" do
       before do
-        allow(Process).to receive(:pid).and_return(123)
         stub_const("Pitchfork::Worker", Class.new)
         allow(ObjectSpace).to receive(:each_object).with(Pitchfork::Worker).
           and_yield(double("Pitchfork::Worker", nr: 3, pid: 122)).
           and_yield(double("Pitchfork::Worker", nr: 3, pid: 122)).
           and_yield(double("Pitchfork::Worker", nr: 3, pid: 122))
+      end
+
+      it { is_expected.to eq("process_id_123") }
+    end
+
+    context "unexpeced pitchfork worker object" do
+      before do
+        stub_const("Pitchfork::Worker", Class.new)
+        allow(ObjectSpace).to receive(:each_object).with(Pitchfork::Worker).
+          and_yield(nil)
       end
 
       it { is_expected.to eq("process_id_123") }
